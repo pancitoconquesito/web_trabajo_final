cb04889a7164f8fa41e09b1b82ec9e4a2310770c
pancitoconquesito <gonzalo.astorga.c@gmail.com>
2021-11-16 03:02:52 -0300

    a

--- Backend/js/app.js
+++ Backend/js/app.js
@@ -1,12 +1,31 @@
 "use strict";
-var http = require('http');
-var hostname = '127.0.0.1';
-var port = 3000;
-var server = http.createServer(function (req, res) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hola Mundo');
+// const http = require('http');
+// const hostname = '127.0.0.1';
+// const port = 3000;
+// const server = http.createServer((req:any, res:any) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hola Mundo');
+// });
+// server.listen(port, hostname, () => {
+//   console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
+// });
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+var POSTGRESQL_ADDON_DB = "brc3eb6i5wutabqzuabc";
+var POSTGRESQL_ADDON_HOST = "brc3eb6i5wutabqzuabc-postgresql.services.clever-cloud.com";
+var POSTGRESQL_ADDON_PASSWORD = "CsR9yeK3vObzxInD0eyB";
+var POSTGRESQL_ADDON_PORT = "5432";
+var POSTGRESQL_ADDON_URI = "postgresql://uysple1axoglntdmlvje:CsR9yeK3vObzxInD0eyB@brc3eb6i5wutabqzuabc-postgresql.services.clever-cloud.com:5432/brc3eb6i5wutabqzuabc";
+var POSTGRESQL_ADDON_USER = "uysple1axoglntdmlvje";
+var POSTGRESQL_ADDON_VERSION = "11";
+var pg = require('pg');
+pg.connect(process.env.POSTGRESQL_ADDON_URI, function (err, client, done) {
 });
-server.listen(port, hostname, function () {
-    console.log("El servidor se est\u00E1 ejecutando en http://" + hostname + ":" + port + "/");
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+app.listen(pg, function () {
+    console.log("Example app listening at http://localhost:" + port);
 });
--- Backend/ts/app.ts
+++ Backend/ts/app.ts
@@ -1,14 +1,41 @@
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req:any, res:any) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola Mundo');
+// const server = http.createServer((req:any, res:any) => {
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hola Mundo');
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
+// });
+
+
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+const POSTGRESQL_ADDON_DB="brc3eb6i5wutabqzuabc"
+const POSTGRESQL_ADDON_HOST="brc3eb6i5wutabqzuabc-postgresql.services.clever-cloud.com"
+const POSTGRESQL_ADDON_PASSWORD="CsR9yeK3vObzxInD0eyB"
+const POSTGRESQL_ADDON_PORT="5432"
+const POSTGRESQL_ADDON_URI="postgresql://uysple1axoglntdmlvje:CsR9yeK3vObzxInD0eyB@brc3eb6i5wutabqzuabc-postgresql.services.clever-cloud.com:5432/brc3eb6i5wutabqzuabc"
+const POSTGRESQL_ADDON_USER="uysple1axoglntdmlvje"
+const POSTGRESQL_ADDON_VERSION="11"
+var pg = require('pg');
+
+pg.connect(process.env.POSTGRESQL_ADDON_URI, function(err:any, client:any, done:any){
+
 });
 
-server.listen(port, hostname, () => {
-  console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
-});
+app.get('/', (req:any, res:any) => {
+  res.send('Hello World!')
+})
+
+app.listen(pg, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
